74164858854634ca7ed65cc00679a15f23df6fb5
attama <kevinejikez@gmail.com>
2022-12-04 17:46:35 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in len(my_string):
-        new_string = my_string.remove('cC')
+        new_string = my_string.translate({ord(i) : None for i in 'cC'})
         return new_string
