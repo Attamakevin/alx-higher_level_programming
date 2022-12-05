f7e564a264e8f081c3aa5c6f6cba4c9e1924f167
attama <kevinejikez@gmail.com>
2022-12-05 15:38:40 -0800

    taske8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/use/bin/python3
+def multiple_returns(sentence):
+    my_turple = ()
+    if len(sentence) == o:
+        my_turple = 0, "None"
+    else:
+        my_turple = len(sentence), sentence[0]
+    return my_turple
