ee43b24ceb6f3cc0840c94a51a989cebf561e92c
attama <kevinejikez@gmail.com>
2022-12-27 08:34:25 -0800

    1-aquare.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""an empty class Square that defines a square"""
+
+
+
+
+
+class Square:
+
+    """an empty class"""
+def __init__ (self, size):
+
+    """initilization of new square.
+    
+    Args:
+
+    size (int): the size of the new square"""
+    self.__size = size
+
+
