a506d7cde09b19c26b2fb0cd8aafefda59593850
attama <kevinejikez@gmail.com>
2022-12-30 14:31:04 -0800

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,16 +3,16 @@
 """an empty class Square that defines a square"""
 
 
-class Square(int):
+class Square:
 
 
     """an empty class"""
 
 
-def __init__(self, size):
+    def __init__(self, size):
 
-    """initilization of new square.
-    Args:
-    size (int): the size of the new square"""
+        """initilization of new square.
+        Args:
+        size (int): the size of the new square"""
 
-    square.__size = size
+        self.__size = size
