f4dbf07434c996b5275346e26fc77067847041bf
attama <kevinejikez@gmail.com>
2022-12-30 15:03:52 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,33 +1,19 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
 
 
 """an empty class Square that defines a square"""
 
 
-
-
-
 class Square:
 
-
-
     """an empty class"""
-
-
-
-    def __init__(self, size = 0):
-
-
+    def __init__(self, size=0):
 
         """initilization of new square.
-
         Args:
-
         size (int): the size of the new square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
