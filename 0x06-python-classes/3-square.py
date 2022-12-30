a95e9f8cd0eb3865d798f08061447e674f9395dc
attama <kevinejikez@gmail.com>
2022-12-30 15:33:10 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""an empty class Square that defines a square"""
+
+
+class Square:
+
+    """an empty class"""
+    def __init__(self, size=0):
+
+        """initilization of new square.
+        Args:
+        size (int): the size of the new square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        """defining area of square"""
+
+        return self.__size**2
