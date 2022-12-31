a88ecdfd87d6dcba29ebbfce9497473369426832
attama <kevinejikez@gmail.com>
2022-12-30 17:19:52 -0800

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+
+"""an empty class Square that defines a square"""
+
+class Square:
+
+    """an empty class"""
+    def __init__(self, size=0):
+
+        """initilization of new square.
+        Args:
+        size (int): the size of the new square"""
+        self.size = size
+
+
+    @property
+    def size(self):
+        """get/set the current size of a square"""
+        return (self.__size)
+
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+
+
+        """defining area of square"""
+
+
+        return (self.__size**2)
+
+    def my_print(self):
+
+        """print  the square with the character #"""
+        for i in range(self.size):
+            print("#",end="")
+            for j in range(self.size):
+                print("#",end="")
+            print()
+            if self.size == 0:
+                print()
+
