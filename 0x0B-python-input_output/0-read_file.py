4d98a63f69627a50bbff14fca46e6e8edab71841
attama <kevinejikez@gmail.com>
2023-01-14 03:51:49 -0800

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """Prints the contents of a UTF8 text file"""
-    with open(filename, mode = 'r', encoding = "utf-8") as f:
-        for line in f:
-            print(line)
+    with open(filename, encoding = "utf-8") as f:
+        print(f.read(), end = "")
