661bb6517a6ec3cd01898bf495f1c67029bdfa3a
attama <kevinejikez@gmail.com>
2023-03-16 13:28:11 -0700

    mandatory tasks done

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- a script that lists all the tables of a database in your MySQL server.
 -- querry to  lists all the tables of a database in your MySQL server
-SHOW TABLES FROM mysql;
+SHOW TABLES;
 
