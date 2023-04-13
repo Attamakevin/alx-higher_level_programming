c97379a22e65b368d4c0ba1bf40cb987ffba15b3
attama <kevinejikez@gmail.com>
2023-04-13 16:09:59 -0700

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add (a, b) {
+  const result = a + b;
+  console.log(result);
+}
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+add(arg1, arg2);
