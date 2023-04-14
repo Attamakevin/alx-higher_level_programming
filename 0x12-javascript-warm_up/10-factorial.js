10a25a41458620b1176e923441b51fef5976bd2b
attama <kevinejikez@gmail.com>
2023-04-13 23:43:32 -0700

    fixed

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    const result = n * factorial(n - 1);
+    return result;
+  }
+}
+
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
