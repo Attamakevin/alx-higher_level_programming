936acea4b8105298be3f4cfd6cd3682b907137d3
attama <kevinejikez@gmail.com>
2023-04-14 11:30:30 -0700

    task 102

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -3,5 +3,6 @@ function addMeMaybe (number, theFunction) {
   const result = number + 1;
   console.log('New value: ' + result);
   return result;
+  theFunction();
 }
 module.exports.addMeMaybe = addMeMaybe;
