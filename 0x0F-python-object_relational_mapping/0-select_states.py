a0535b93012ddcfae704355933b18ce49fd76d52
attamakevin <kevinejikez@gmail.com>
2023-04-22 16:04:25 -0700

    fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,18 +11,17 @@ import sys
 
 if __name__ == '__main__':
 
-
     # Get MySQL username, password, and database name as command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     # Connect to MySQL server running on localhost at port 3306
-    conn = MySQLdb.connect(host="localhost",
-                             port=3306, 
-                             user=username,
-                             passwd=password,
-                             db=database)
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=username,
+                           passwd=password,
+                           db=database)
 
     # Create cursor to execute SQL queries
     cursor = conn.cursor()
