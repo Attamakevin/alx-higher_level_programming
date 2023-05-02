b4ea0ee9a2772e95c33d04f150b9ac89ea848a91
attamakevin <kevinejikez@gmail.com>
2023-05-02 01:35:21 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Check if URL is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL"
-  exit 1
-fi
 # Send a request and get the size of the response body in bytes
 curl -s "$1"| wc -c
